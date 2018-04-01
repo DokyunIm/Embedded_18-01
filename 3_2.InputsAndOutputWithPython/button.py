06cb65ffb8282716054d03496e0046951270a584
DokyunIm <imjoshua9316@gmail.com>
2018-04-01 04:44:24 -0900

    change Button GPIO number

--- 3_2.InputsAndOutputWithPython/button.py
+++ 3_2.InputsAndOutputWithPython/button.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
 
 count = 0
 
 while True:
-    inputValue = GPIO.input(24)
+    inputValue = GPIO.input(23)
     if(inputValue == True):
         count = count +1
         print("Button pressed" + str(count) + "times.")
-    time.sleep(.01)
+    time.sleep(.3)
